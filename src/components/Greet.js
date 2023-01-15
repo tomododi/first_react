ea2a663913fa3b9440c13fdd38a37f5df8d9b4ff
tomododi <tomdudek@hotmail.com>
2023-01-15 01:12:19 +0100

    created first test class component and function component

---
+++ src/components/Greet.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from 'react';
+
+export const Greet = () => <h1>Hello Tom</h1>;
+
+//export default Greet;
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return <h1>Class Component</h1>;
+  }
+}
+
+export default Welcome;
